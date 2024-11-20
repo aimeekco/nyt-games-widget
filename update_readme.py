549d6db8d5db0944ce0b6406f3373bf6f7a5e814
Aimee Co <aimeekco@gmail.com>
2024-11-19 23:43:52 -0800

    update readme function

--- update_readme.py
+++ update_readme.py
@@ -1,12 +1,20 @@
-from process_stats import load_stats, process_stats
-
 def update_readme():
     stats = load_stats()
     content = process_stats(stats)
 
-    # write to README file
+    # read current README
+    with open("README.md", "r") as f:
+        readme = f.read()
+
+    # replace placeholder <!-- NYT-STATS --> with the new content
+    updated_readme = readme.replace(
+        "<!-- NYT-STATS -->",
+        content
+    )
+
+    # write updated content back to the README file
     with open("README.md", "w") as f:
-        f.write(content)
+        f.write(updated_readme)
 
     print("README.md updated with the latest stats!")
 
